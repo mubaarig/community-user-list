2dd979672b31c45b1358fe49f8d556d948053df7
Mubarig Cisman <mubo1@gmail.com>
2022-03-30 20:30:40 +0200

    data.js

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+      id: 1,
+      name: 'Dale Yates',
+      age: 33,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    },
+    {
+      id: 2,
+      name: 'Hester Hogan',
+      age: 45,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+      id: 3,
+      name: 'Larry Page',
+      age: 36,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+      id: 4,
+      name: 'Tim Walsh',
+      age: 34,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+      id: 5,
+      name: 'Ali Ramadi',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+  ];
+  
