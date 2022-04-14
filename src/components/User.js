d339d0810abb5b729d92d8444f230b525f78e395
Mubarig Cisman <mubo1@gmail.com>
2022-04-15 01:34:46 +0200

    User Component

---
+++ src/components/User.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+
+const User = ({ id, name, location, image }) => {
+    return (
+        <div class="flex-container">
+            <div class="flex-items">
+                <article key={id} className="user-card person">
+                    <div className="flex-item">
+                        <img src={image} />
+                    </div>
+                    <div className="flex-item">
+                        <h4>{name}</h4>
+                        <h6>{location}</h6>
+                    </div>
+                </article>
+            </div>
+        </div>
+    );
+}
+
+
+export default User;
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,32 +1,41 @@
 import React, { useState } from "react";
+import User from './User';
 
 
 const Users = ({ users }) => {
     return (
-            <div class="flex-container">
-                {users.map((user) => {
-
-                    const { id, name, location, image } = user;
-                    return (
-                        <div class="flex-items">
-                            <article key={id} className="user-card person">
-                                <div className="flex-item">
-                                    <img src={image} />
-                                </div>
-                                <div className="flex-item">
-                                    <h4>{name}</h4>
-                                    <h6>{location}</h6>
-                                </div>                                
-                            </article>
-                        </div>
-
-
-                    );
-
-                })}          
+        <div class="flex-container">
+            {users.map((user) => {
+                return <User key={user.id} {...user}/>
+            })}
         </div>
     );
 }
 
 
 export default Users;
+
+
+// const Users = ({ users }) => {
+//     return (
+//         <div class="flex-container">
+//             {users.map((user) => {
+
+//                 const { id, name, location, image } = user;
+//                 return (
+//                     <div class="flex-items">
+//                         <article key={id} className="user-card person">
+//                             <div className="flex-item">
+//                                 <img src={image} />
+//                             </div>
+//                             <div className="flex-item">
+//                                 <h4>{name}</h4>
+//                                 <h6>{location}</h6>
+//                             </div>
+//                         </article>
+//                     </div>
+//                 );
+//             })}
+//         </div>
+//     );
+// }
